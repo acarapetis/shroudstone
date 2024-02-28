df3664f0ed3109402a542d9eba0d23c3f423cfc3
Anthony Carapetis <anthony.carapetis@gmail.com>
2024-02-28 21:38:00 +1100

    Add autogenerated stormgateworld API client
    Gonna be handy to have type hints for some of it

---
+++ shroudstone/sgw_api.py
@@ -0,0 +1,12 @@
+from shroudstone.stormgateworld import ApiClient
+from shroudstone.stormgateworld.api import players_api, matches_api
+from shroudstone import __version__
+
+USER_AGENT = f"shroudstone v{__version__}"
+"""User-Agent to use in requests to the API"""
+
+client = ApiClient()
+client.user_agent = USER_AGENT
+
+Players = players_api.PlayersApi(api_client=client)
+Matches = matches_api.MatchesApi(api_client=client)
---
+++ shroudstone/stormgateworld/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from shroudstone.stormgateworld.api.leaderboard_entries_api import LeaderboardEntriesApi
+from shroudstone.stormgateworld.api.leaderboards_api import LeaderboardsApi
+from shroudstone.stormgateworld.api.matches_api import MatchesApi
+from shroudstone.stormgateworld.api.players_api import PlayersApi
+from shroudstone.stormgateworld.api.statistics_api import StatisticsApi
+
